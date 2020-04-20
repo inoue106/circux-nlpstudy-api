07cdd8fc369b27cf5cff0c861829a9cf17946ea4
inoue106 <toru.inoue@circu.co.jp>
2020-04-20 17:08:41 +0900

    improved

--- src/server.py
+++ src/server.py
@@ -75,13 +75,57 @@ def top5():
             'similarity': s
         })
 
-@app.route('/wnjpn', methods=["GET"])
-def wnjpn():
-    cur = conn.execute("select name from sqlite_master where type='table'")
+@app.route('/wnjpn/<word>', methods=["GET"])
+def wnjpn(word):
+    # 問い合わせしたい単語がWordnetに存在するか確認する
+    cur = conn.execute("select wordid from word where lemma='%s'" % word)
+    word_id = 99999999  #temp 
     for row in cur:
-        print(row)
+        word_id = row[0]
+
+    # Wordnetに存在する語であるかの判定
+    if word_id==99999999:
+        print("「%s」は、Wordnetに存在しない単語です。" % word)
+        return jsonify({
+                'status': 'NG',
+                'message': ("「%s」は、Wordnetに存在しない単語です。" % word)
+            })
+    else:
+        print("【「%s」の類似語を出力します】\n" % word)
+
+    # 入力された単語を含む概念を検索する
+    cur = conn.execute("select synset from sense where wordid='%s'" % word_id)
+    synsets = []
+    for row in cur:
+        synsets.append(row[0])
+
+    # 概念に含まれる単語を検索して画面出力する
+    results = []
+    no = 1
+    for synset in synsets:
+        cur1 = conn.execute("select name from synset where synset='%s'" % synset)
+        for row1 in cur1:
+            print("%sつめの概念 : %s" %(no, row1[0]))
+        cur2 = conn.execute("select def from synset_def where (synset='%s' and lang='jpn')" % synset)
+        sub_no = 1
+        for row2 in cur2:
+            print("意味%s : %s" %(sub_no, row2[0]))
+            sub_no += 1
+        cur3 = conn.execute("select wordid from sense where (synset='%s' and wordid!=%s)" % (synset,word_id))
+        sub_no = 1
+        for row3 in cur3:
+            target_word_id = row3[0]
+            cur3_1 = conn.execute("select lemma from word where wordid=%s" % target_word_id)
+            for row3_1 in cur3_1:
+                results.append(row3_1[0])
+                print("類義語%s : %s" % (sub_no, row3_1[0]))
+                sub_no += 1
+        print("\n")
+        no += 1
+
     return jsonify({
-            'status': 'OK'
+            'status': 'OK',
+            'result': results
         })
 
 if __name__ == "__main__":
